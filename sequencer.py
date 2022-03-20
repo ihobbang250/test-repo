e43c834568574cc4f76a47f8cd68c9e175e07485
hobbang_250 <48835391+Hoyoung250@users.noreply.github.com>
2022-03-21 01:21:01 +0900

    Update sequencer.py

--- sequencer.py
+++ sequencer.py
@@ -5,13 +5,14 @@ class Sequencer:
         self.maxValue = maxValue
     def __len__(self):
         return self.maxValue
-    def __getitem__(self, index):
+    def __getitem__(self, index): #메서드 재정의/인덱스로 아이템 값접근
         if 0 < index <= self.maxValue:
             return index * 10 # element index * 10
         else:
             raise IndexError('index out of range')
     def __contains__(self, item):
         return 0 < item <= self.maxValue
-
-s = Sequencer(5) #i.g) 1~5 sequencerc
-t = Sequencer(6) #6 parameter
+  
+num = int(input()) #숫자입력받기
+s = Sequencer(num)
+print([s[i] for i in range(1, num+1)]) #s객체를 통해 얻을수 있는 값
